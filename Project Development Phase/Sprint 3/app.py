8972bb5c4f9331c8766e3aee9e5f0fcbea6d359a
Sabari37 <113204051+Sabari37@users.noreply.github.com>
2022-11-20 18:28:02 +0530

    Update app.py

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -19,7 +19,7 @@ mail = Mail(app)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'team06inventory@gmail.com'
-app.config['MAIL_PASSWORD'] = 'pjqnmjtrdlqkjqfj'
+
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
